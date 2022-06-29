369d0f36cd0a808757fb35d5c54e0d9697ef127e
wzw <2537366933@qq.com>
2022-06-29 14:51:08 +0800

    Initial commit

---
+++ src/main/java/com/wzw/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.wzw.git;
+
+/**
+ * @Author: wzw
+ * @Date: Created in 11:28 2022/6/29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
